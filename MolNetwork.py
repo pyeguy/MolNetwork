7cf57ef3d05a961d87e622c5d0a2166644a88d7e
unknown <cameron.pye@gmail.com>
2016-08-31 11:55:13 -0700

    added fptype and metric arguments

--- MolNetwork.py
+++ MolNetwork.py
@@ -13,7 +13,17 @@ import textwrap
 #fps = [[AllChem.GetMorganFingerprint(Chem.MolFromSmiles(smi),4,useChirality=True),name] for smi,name in smilist]
 #scores = [[name1,name2,Chem.DataStructs.DiceSimilarity(fp1,fp2)] for name1,fp1 in fps for name2,fp2, in fps]
 
+FPMETHODS = {
+    'ECFC4' : lambda x:AllChem.GetMorganFingerprint(x,4,useChirality=True),
+    'ECFP4' : lambda x:AllChem.GetMorganFingerprintAsBitVect(x,4,useChirality=True),
+}
 
+METRICS = {
+    'DICE' : AllChem.DataStructs.DiceSimilarity,
+    'Tanimoto' : AllChem.DataStructs.TanimotoSimilarity,
+    'Cosine' : AllChem.DataStructs.CosineSimilarity,
+    'Sokal' : AllChem.DataStructs.SokalSimilarity,
+}
 
 def getNummols(infile):
     infile_components = infile.split('.')
@@ -47,10 +57,11 @@ def genMollist(infile):
             yield [mol,name]
 
 
-def genFps(infile):
+def genFps(infile,fptype):
+    fpmethod = FPMETHODS[fptype]
     infile_components = infile.split('.')
     try:
-        with open(infile_components[0]+'.pkl','rb') as fin:
+        with open('{}_{}.pkl'.format(infile_components[0],fptype),'rb') as fin:
             print('Loading old FP pkl file')
             fps = pickle.load(fin)
             print('Done Loading FPs')
@@ -61,12 +72,13 @@ def genFps(infile):
         fps = []
         pbar = progressbar.ProgressBar(max_value=nummols)
         for mol,name in pbar(molgen):
-            fps.append([name,AllChem.GetMorganFingerprint(mol,4,useChirality=True)])
-        with open(infile_components[0]+'.pkl','wb') as fout:
+            fps.append([name,fpmethod(mol)])
+        with open('{}_{}.pkl'.format(infile_components[0],fptype),'wb') as fout:
             pickle.dump(fps,fout)
     return fps 
 
-def full_simtable(fps):
+def full_simtable(fps,metric):
+    metric_method = METRICS[metric]
     print("Making Sim Table")
     i = 1
     scores = []
@@ -75,7 +87,7 @@ def full_simtable(fps):
     for name1,fp1 in bar(fps):
         for name2,fp2 in fps[i:]:
             # if j > i :
-            score = Chem.DataStructs.DiceSimilarity(fp1,fp2)
+            score = metric_method(fp1,fp2)
             if score >= args.thresh:
                 non_sings.add(name1)
                 non_sings.add(name2)
@@ -91,7 +103,8 @@ def full_simtable(fps):
         scores.extend([[sing,sing,1.0] for sing in sings])
     return scores
 
-def sample_scores(fps,thresh,samplesize):
+def sample_scores(fps,thresh,samplesize,metric):
+    metric_method = METRICS[metric]
     if samplesize > len(fps):
         raise Exception('Sample Size cannot exceede number of molecules')
     scores = []
@@ -100,7 +113,7 @@ def sample_scores(fps,thresh,samplesize):
     for name1,fp1 in pbar(fps):
         for name2,fp2 in sample(fps,samplesize):
             if fp1 != fp2:
-                score = Chem.DataStructs.DiceSimilarity(fp1,fp2)
+                score = metric_method(fp1,fp2)
                 if score >= thresh:
                     non_sings.add(name1)
                     non_sings.add(name2)
@@ -115,19 +128,20 @@ def sample_scores(fps,thresh,samplesize):
         scores.extend([[sing,sing,1.0] for sing in sings])  
     return scores
 
-def negSAR(full_filename,hits_fps):
+def negSAR(full_filename,hits_fps,metric):
+    metric_method = METRICS[metric]
     # full_filename_components = full_filename.split('.')
     neg_sar_scores = []
     neg_sar_names = set()
     # full_fps = {name:fp for name,fp in genFps(full_mollist)}
     print('Getting Fingerprints of the full library... ')
-    full_fps = genFps(full_filename)
+    full_fps = genFps(full_filename,args.fptype)
     pbar = progressbar.ProgressBar()
     print('Making Neg SAR comparisons')
     for hit_name,hit_fp in pbar(hits_fps):
         for neg_name,neg_fp in full_fps:
             if hit_name != neg_name:
-                score = Chem.DataStructs.DiceSimilarity(hit_fp,neg_fp)
+                score = metric_method(hit_fp,neg_fp)
                 if score >= args.thresh:
                     neg_sar_scores.append([hit_name,neg_name,score])
                     neg_sar_names.add(neg_name)
@@ -168,20 +182,25 @@ if __name__ == '__main__':
     parser.add_argument('--samplesize','-s',help='number of compounds to sample when generating the sim table (for large # of compounds)',type=int)
     parser.add_argument('--singletons',action='store_true',help='include self-loop for all singletons')
     parser.add_argument('--negsar',help='File with all compounds to be clustered w/hits. will output a second file with hit attaribute')
+    parser.add_argument('--fptype',help='the type of fp to be used (these are the Morgan Algo. equivalents)',choices=['ECFC4','ECFP4'],default='ECFC4')
+    parser.add_argument('--metric',help='Distance/Simiarity Metric to use',choices=['DICE','Tanimoto','Cosine'],default='DICE')
     args = parser.parse_args()
 
     infile_components = args.infile.split('.')
     outfile_components = args.outfile.split('.')
 
-    fps = genFps(args.infile)
+    if args.metric in {'Cosine'} and args.fptype in {'ECFP4'}:
+        raise Exception("Unspported Metric and FP combination : <{}> & <{}>".format(args.metric,args.fptype))
+
+    fps = genFps(args.infile,args.fptype)
     
     if args.samplesize:
-        scores = sample_scores(fps,args.thresh,args.samplesize)
+        scores = sample_scores(fps,args.thresh,args.samplesize,args.metric)
     else:
-        scores = full_simtable(fps)
+        scores = full_simtable(fps,args.metric)
 
     if args.negsar:
-        neg_sar_scores, neg_sar_names = negSAR(args.negsar,fps)
+        neg_sar_scores, neg_sar_names = negSAR(args.negsar,fps,args.metric)
         print('Neg SAR compounds: {}'.format(len(neg_sar_names)))
         scores.extend(neg_sar_scores)
         hitmap = [[neg,0] for neg in neg_sar_names]
